3258a291ca30bce5fcfaf50dc3637a0ec64a9b26
Willyan <wkinhaamaral@gmail.com>
2023-09-26 18:36:56 -0300

    aula-03

--- aula-03/tests/tasks.test.ts
+++
@@ -1,9 +0,0 @@
-describe("arquivo testado", () => {
-    it("/calculator", () => {
-        expect(3).toEqual(3);
-    });
-  
-    it("returns false for invalid params", () => {
-      // Aqui vai o código desse teste
-    });
-  });
