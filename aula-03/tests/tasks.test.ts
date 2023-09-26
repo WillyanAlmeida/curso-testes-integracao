5360b4da07419b2396fea246e12761bcb5e9f5b9
Willyan <wkinhaamaral@gmail.com>
2023-09-26 18:31:12 -0300

    fisrts commit

---
+++ aula-03/tests/tasks.test.ts
@@ -0,0 +1,9 @@
+describe("arquivo testado", () => {
+    it("/calculator", () => {
+        expect(3).toEqual(3);
+    });
+  
+    it("returns false for invalid params", () => {
+      // Aqui vai o código desse teste
+    });
+  });
