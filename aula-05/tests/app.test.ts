acd0f405bcb783c08bb8caebb9576546f24205f0
Willyan <wkinhaamaral@gmail.com>
2023-09-27 13:35:32 -0300

    aula6

--- aula-05/tests/app.test.ts
+++ aula-05/tests/app.test.ts
@@ -10,8 +10,18 @@ describe("API test", () => {
     expect(status).toBe(200);
     expect(text).toBe("OK!");
   })
-  it("should return 200 when ask /fibonacci", async () => {
-    const result = await api.get("/fibonacci");
+  it('should return 400 when ask/fibonacci has an invalid query', async () => {
+    const result = await api.get("/fibonacci?elements=x");
+    expect(result.status).toBe(400);
+
+    const result2 = await api.get("/fibonacci?elements=0");
+    expect(result2.status).toBe(400);
+  
+   
+  })
+  it("test return values /fibonacci", async () => {
+    const result = await api.get("/fibonacci?elements=5");
+    expect(result.body).toEqual([0, 1, 1, 2, 3]);
     
   })
 })
--- aula-06/tests/app.test.ts
+++ aula-06/tests/app.test.ts
@@ -5,5 +5,20 @@ import app from "./../src/app";
 const api = supertest(app);
 
 describe("API test", () => {
-  // TODO
+  it("should return 200 when ask /health", async () => {
+    const { status, text } = await api.get("/health");
+    expect(status).toBe(200);
+    expect(text).toBe("OK!");
+  })
+  it("should be return stauts 200", async()=>{
+    const result = await api.get("/event")
+    expect(result.body).toEqual(
+      expect.objectContaining({
+        id: expect.any(Number),
+        image: expect.any(String),
+        date: expect.any(String)
+      })
+    )
+    expect(result.status).toBe(200) 
+  });
 });
